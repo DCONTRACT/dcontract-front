29ec59f9b31210bf960136de0e6defbcb5e26aa3
SidikiSidibe <sidikisidibe81@gmail.com>
2021-04-25 04:36:10 +0200

    add menu and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,23 @@
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+  ]
+})
+export class ComponentsModule { }
